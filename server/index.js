9c4bfd51c4dc863a47f5d8aaa886d6b3bf3f2319
Dragos Nedelcu <dragos.nedelcu@misterspex.de>
2019-12-20 13:35:47 +0100

    indentation

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
-require('ignore-styles') // ignore CSS files imported using import syntax
-require('@babel/register')({
-    ignore: [/(node_modules)/],
-    presets: ['@babel/preset-env', '@babel/preset-react']
-  })
-require('./server')
+require("ignore-styles"); // ignore CSS files imported using import syntax
+require("@babel/register")({
+  ignore: [/(node_modules)/],
+  presets: ["@babel/preset-env", "@babel/preset-react"]
+});
+require("./server");
--- server/server.js
+++ server/server.js
@@ -1,46 +1,41 @@
-import fs from "fs"
-import path from "path"
+import fs from "fs";
+import path from "path";
 
+import express from "express";
+import React from "react";
+import ReactDOMServer from "react-dom/server";
 
-import express from "express"
-import React from "react"
-import ReactDOMServer from "react-dom/server"
+import App from "../src/App";
 
-import App from "../src/App"
+const PORT = 8080;
+const app = express();
 
-const PORT = 8080
-const app = express()
-
-
-const router = express.Router()
-
+const router = express.Router();
 
 const serverRender = (req, res, next) => {
-    fs.readFile(path.resolve("./build/index.html"), 'utf-8', (err, data) => {
-        if(err) {
-            console.error(err)
-            return res.status(500).send("An error ocurred")
-        }
+  fs.readFile(path.resolve("./build/index.html"), "utf-8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("An error ocurred");
+    }
 
-        return res.send(
-            data.replace(
-                '<div id="root"></div>',
-                `<div id="root">${ReactDOMServer.renderToString(<App />)}</div>`
-            )
-        )
-    })
-}
+    return res.send(
+      data.replace(
+        '<div id="root"></div>',
+        `<div id="root">${ReactDOMServer.renderToString(<App />)}</div>`
+      )
+    );
+  });
+};
 
-router.use('^/$', serverRender)
-
+router.use("^/$", serverRender);
 
 router.use(
-    express.static(path.resolve(__dirname, '..', 'build'), {maxAge: '30d'})
-)
+  express.static(path.resolve(__dirname, "..", "build"), { maxAge: "30d" })
+);
 
-app.use(router)
-
+app.use(router);
 
 app.listen(PORT, () => {
-    console.log(`SSR running on port ${PORT}`)
-})
+  console.log(`SSR running on port ${PORT}`);
+});
